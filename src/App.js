8d4c1862b4e397d6567cb9625461f59ce65084f4
Suzanne Cabral <suzannecabral@live.com>
2020-09-18 08:17:32 -0700

    Initial commit, installed cypress and axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import OrderForm from './OrderForm'
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+
+      <OrderForm />
     </>
   );
 };
---
+++ src/OrderForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrderForm(props){
+    return(
+        <div id="orderForm">
+            <h2>Order form goes here</h2>
+        </div>
+    )
+}
