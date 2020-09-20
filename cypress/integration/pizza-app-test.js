c00ed5cda2ba8ebf1001a06606617013938b779b
Suzanne Cabral <suzannecabral@live.com>
2020-09-20 14:11:12 -0700

    Testing mvp 3 of 3 complete

--- cypress/integration/pizza-app-test.js
+++ cypress/integration/pizza-app-test.js
@@ -31,17 +31,22 @@ describe('Visit the page',()=>{
 
 //arrange
 describe('Add text to the text box',()=>{
+
     //act
     it('Finds a text box named "name"',()=>{
         //assert
         cy.get('input[name=name]').should('be.visible')
     })
 
+    //act
     it('does not find a radio button',()=>{
+        //assert
         cy.get('input[type=radio]').should('not.be.visible')
     })
 
+    //act
     it('types text into the input',()=>{
+        //assert
         cy.get('input[name=name]')
             .type('Cypress Bot')
             .should('have.value','Cypress Bot')
@@ -52,5 +57,54 @@ describe('Add text to the text box',()=>{
 // [x] successful (1:48pm)
 
 //-------------------------------------
-//MVP 2: you can add text to the text box
+//MVP 2: you can select multiple toppings
 //-------------------------------------
+
+//arrange
+describe('Add multiple toppings to the order',()=>{
+    //act
+    it('finds a checkbox on the page',()=>{
+        //assert
+        cy.get('input[type=checkbox]').should('be.visible')
+    })
+
+    //act
+    it('checks 3 boxes',()=>{
+        //assert
+        cy.get('input[name=cheese]')
+            .click()
+            .should('be.checked')
+        cy.get('input[name=pepperoni]')
+        .click()
+        .should('be.checked')
+    cy.get('input[name=mushrooms]')
+        .click()
+        .should('be.checked')
+    })
+})
+
+// [x] successful (1:59pm)
+
+//-------------------------------------
+//MVP 3: you can submit the form
+//(after selecting all required elements)
+//-------------------------------------
+
+describe('Select required fields and submit the form',()=>{
+    it('Sees an "add to order" button',()=>{
+        cy.contains('Add to Order')
+    })
+    it("Order button is disabled before filling all fields", ()=>{
+        cy.contains('Add to Order')
+            .should('be.disabled')
+    })
+    it('selects a pizza size',()=>{
+        cy.get('select[name=pizzaSize]')
+            .select('Small')
+    })
+    it('submits the form',()=>{
+        cy.contains('Add to Order')
+        .click()
+    })
+})
+// [x] successful (2:10pm)
